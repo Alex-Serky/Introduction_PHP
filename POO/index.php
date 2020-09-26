22bb3db5d71238f8faa9f618f79cf4ebd2a266a8
Alex-Serky <aserky@gmail.com>
2020-09-26 18:12:32 +0200

    POO

---
+++ POO/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+      <a href="presentation.php?nom=Alexis&age=45">Présentation</a>
+
+  </body>
+</html>
---
+++ POO/presentation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php var_dump($_GET); ?>
+
+    <!-- <h1><?php echo "Hello! Moi, cest ". $_GET['nom']." et j'ai ". $_GET['age']. "ans.";?></h1> -->
+
+  </body>
+</html>
