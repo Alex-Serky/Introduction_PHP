4aec54aea6330a0590dacc32f30ef18959e64cc4
Alex-Serky <aserky@gmail.com>
2020-09-26 18:04:51 +0200

    POST

---
+++ POST/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="index.html" method="post">
+      Nom: <input type="text" name="npm"><br>
+      Email: <input type="text" name="email"><br>
+      <input type="submit">
+    </form>
+  </body>
+</html>
