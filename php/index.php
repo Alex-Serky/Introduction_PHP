58cd576c0e0ee43eb8641a47fbfed9c2054feeaf
Alex-Serky <aserky@gmail.com>
2020-09-26 17:43:11 +0200

    Condition if elseif

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$motivation = 50;
+
+if ($motivation < 3) {
+  echo "Ouh la la! Il faut se motiver";
+}elseif ($motivation > 7) {
+  echo "Ton cas n'est pas grave... Ressaisis-toi!";
+}else {
+  echo "C'est génial! COntinues dans ce sens.";
+}
+
+
+ ?>
