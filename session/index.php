1e0606225b41808ffc5645905a911c9948a6dcf3
Alex-Serky <aserky@gmail.com>
2020-09-26 18:09:37 +0200

    La Session

---
+++ session/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+$_SESSION["nom"] = "Alexis";
+?>
+
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <a href="presentation.php">Qui es-tu?</a>
+  </body>
+</html>
---
+++ session/presentation.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <?php var_dump($_SESSION) ?>
+  </body>
+</html>
